11b29daf84c22a12c43896345d570befaf1f8489
reza derikvand <reza.derikvand@gmail.com>
2024-05-23 15:03:34 +0330

    Initial commit

---
+++ app/src/main/java/drk/reza/myjetpackcomposecomponents/MainActivity.kt
@@ -0,0 +1,47 @@
+package drk.reza.myjetpackcomposecomponents
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import drk.reza.myjetpackcomposecomponents.ui.theme.MyJetpackComposeComponentsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyJetpackComposeComponentsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyJetpackComposeComponentsTheme {
+        Greeting("Android")
+    }
+}
